7f78a54de6eda5c9bcc8ea15a4ea405d6b08df51
Ege Çalışkan <68867259+EgeCaliskan@users.noreply.github.com>
2024-07-04 20:53:22 +0300

    Update 1.32 - General Accumulator.rkt

--- Section 1/1.32 - General Accumulator.rkt
+++ Section 1/1.32 - General Accumulator.rkt
@@ -4,10 +4,16 @@
       null-value
       (combiner (term a) (accumulate combiner term (next a) next b null-value))))
 
+(define (accumulate-iter combiner term a next b result)
+  (if (> a b)
+      result
+      (accumulate combiner term (next a) next b (combiner a result))))
 
 (define (factorial n)
   (define (multiply x y) (* x y))
   (define (identity x) x)
 
-  (accumulate multiply identity 1 inc n 1)
+  (accumulate-iter multiply identity 1 inc n 1)
   )
+
+(factorial 10)
