d8e09c37dc7122ffde5cdfd53a54390a4ca1c762
Ege Çalışkan <68867259+EgeCaliskan@users.noreply.github.com>
2025-02-07 15:54:35 +0100

    Add files via upload

---
+++ Section2/2.9 - Widths Of Intervals.rkt
@@ -0,0 +1,49 @@
+#lang sicp
+
+(define (make-interval x y)
+  (if (> x y)
+      (cons y x)
+      (cons x y)
+      )
+  )
+(define (upper-bound x) (cdr x))
+(define (lower-bound x) (car x))
+
+(define (add-interval a b)
+  (make-interval (+ (lower-bound a) (lower-bound b)) (+ (upper-bound a) (upper-bound b))) 
+  )
+
+(define (mult-interval a b)
+  (let ((p1 (* (lower-bound a) (lower-bound b))) (p2 (* (lower-bound a) (upper-bound b))) (p3 (* (upper-bound a) (lower-bound b))) (p4 (* (upper-bound a) (upper-bound b)) ))
+
+    (make-interval (min p1 p2 p3 p4) (max p1 p2 p3 p4))
+    )
+  )
+
+
+(define (sub-interval a b)
+  (define m (make-interval (- (upper-bound b)) (- (lower-bound b))))
+  (add-interval a m)
+  )
+
+
+(define (width a)
+  (/ (- (upper-bound a) (lower-bound a)) 2)
+  )
+
+;Width of the sum of two intervals is equal to the sum of their individual widths.
+;Let I1 (a,b) , I2 (c,d), I3 = I1 + I2 = (a+c, b+d)
+;Let W represent the width of an interval.
+;W(I1) = (b - a)/ 2, W(I2) = (d - c) / 2.
+;W(I1) + W(I2) = (b+d - (a+c))/2 = W(I3)
+
+;Width of the multiplication of two intervals might not be equal to the multiplication of their individual widths.
+;Example: I1 = (-5 -2) , I2 = (-10, 7)
+
+(define I1 (make-interval -5 -2))
+(define I2 (make-interval -10 7))
+
+(width I1)
+(width I2)
+(width (mult-interval I1 I2))
+(* (width I1) (width I2) )
