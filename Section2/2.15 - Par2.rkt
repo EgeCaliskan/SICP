322619a602ff5e6867031775c3665b07c7723382
Ege Çalışkan <68867259+EgeCaliskan@users.noreply.github.com>
2025-02-16 12:12:07 +0300

    Create 2.15 - Par2.rkt

---
+++ Section2/2.15 - Par2.rkt
@@ -0,0 +1,67 @@
+#lang sicp
+
+(define (make-interval x y)
+  (if (> x y)
+      (cons y x)
+      (cons x y)
+      )
+  )
+(define (upper-bound x) (cdr x))
+(define (lower-bound x) (car x))
+
+(define (add-interval a b)
+  (make-interval (+ (lower-bound a) (lower-bound b)) (+ (upper-bound a) (upper-bound b))) 
+  )
+
+
+
+(define (mult-interval a b)
+  (let ((p1 (* (lower-bound a) (lower-bound b))) (p2 (* (lower-bound a) (upper-bound b))) (p3 (* (upper-bound a) (lower-bound b))) (p4 (* (upper-bound a) (upper-bound b)) ))
+
+    (make-interval (min p1 p2 p3 p4) (max p1 p2 p3 p4))
+    )
+  )
+
+(define (div-interval x y)
+  (begin
+    (if (or (= (lower-bound y) 0) (= (upper-bound y) 0))
+        (display "Cannot divide by zero.")
+        (mult-interval x 
+                       (make-interval 
+                        (/ 1.0 (upper-bound y)) 
+                        (/ 1.0 (lower-bound y))))
+        ))
+  )
+
+(define (make-center-percent point percent)
+  (make-interval (* point (- 100 percent) 0.01) (* point (+ 100 percent) 0.01))
+  )
+
+(define (par1 r1 r2)
+  (div-interval 
+   (mult-interval r1 r2)
+   (add-interval r1 r2)))
+
+(define (par2 r1 r2)
+  (let ((one (make-interval 1 1)))
+    (div-interval 
+     one
+     (add-interval 
+      (div-interval one r1) 
+      (div-interval one r2)))))
+
+(define (percent interval)
+  (- (/ (upper-bound interval) (center interval) 0.01 ) 100)
+  )
+
+(define (center i)
+  (/ (+ (lower-bound i) 
+        (upper-bound i)) 
+     2))
+(define R1 (make-center-percent 200 10 ))
+(define R2 (make-center-percent 520 3))
+
+
+(percent (par1 R1 R2))
+(percent (par2 R1 R2))
+; It can be seen that par2 produces an interval with lower uncertainty.
